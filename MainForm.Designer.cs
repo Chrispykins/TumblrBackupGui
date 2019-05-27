fa8e261ac632e11d2970fa59cb016c773fe6268d
Chrispykins <fermion@email.com>
2019-05-26 22:25:06 -0700

    Initial commit

---
+++ MainForm.Designer.cs
@@ -0,0 +1,148 @@
+﻿namespace TumblrBackupGui
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.label1 = new System.Windows.Forms.Label();
+            this.OutputFolderText = new System.Windows.Forms.TextBox();
+            this.OutputFolderButton = new System.Windows.Forms.Button();
+            this.BlogNamePanel = new System.Windows.Forms.Panel();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.DownloadButton = new System.Windows.Forms.Button();
+            this.ErrorText = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(15, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(124, 22);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Output Folder:";
+            // 
+            // OutputFolderText
+            // 
+            this.OutputFolderText.Location = new System.Drawing.Point(19, 34);
+            this.OutputFolderText.Name = "OutputFolderText";
+            this.OutputFolderText.Size = new System.Drawing.Size(405, 28);
+            this.OutputFolderText.TabIndex = 1;
+            // 
+            // OutputFolderButton
+            // 
+            this.OutputFolderButton.BackgroundImage = global::TumblrBackupGui.Properties.Resources.folder;
+            this.OutputFolderButton.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.OutputFolderButton.Location = new System.Drawing.Point(430, 27);
+            this.OutputFolderButton.Name = "OutputFolderButton";
+            this.OutputFolderButton.Size = new System.Drawing.Size(40, 40);
+            this.OutputFolderButton.TabIndex = 2;
+            this.OutputFolderButton.UseVisualStyleBackColor = true;
+            this.OutputFolderButton.Click += new System.EventHandler(this.OutputFolderButton_Click);
+            // 
+            // BlogNamePanel
+            // 
+            this.BlogNamePanel.AutoScroll = true;
+            this.BlogNamePanel.Location = new System.Drawing.Point(16, 30);
+            this.BlogNamePanel.Name = "BlogNamePanel";
+            this.BlogNamePanel.Size = new System.Drawing.Size(634, 297);
+            this.BlogNamePanel.TabIndex = 4;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.BlogNamePanel);
+            this.groupBox1.Font = new System.Drawing.Font("Gentium Basic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.ForeColor = System.Drawing.Color.White;
+            this.groupBox1.Location = new System.Drawing.Point(19, 90);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(656, 333);
+            this.groupBox1.TabIndex = 5;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Blog Names";
+            // 
+            // DownloadButton
+            // 
+            this.DownloadButton.Font = new System.Drawing.Font("GENUINE", 13.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(2)));
+            this.DownloadButton.ForeColor = System.Drawing.Color.Black;
+            this.DownloadButton.Location = new System.Drawing.Point(542, 488);
+            this.DownloadButton.Name = "DownloadButton";
+            this.DownloadButton.Size = new System.Drawing.Size(127, 41);
+            this.DownloadButton.TabIndex = 6;
+            this.DownloadButton.Text = "Download";
+            this.DownloadButton.UseVisualStyleBackColor = true;
+            this.DownloadButton.Click += new System.EventHandler(this.DownloadButton_Click);
+            // 
+            // ErrorText
+            // 
+            this.ErrorText.AutoSize = true;
+            this.ErrorText.Font = new System.Drawing.Font("Gentium Basic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ErrorText.ForeColor = System.Drawing.Color.LightSalmon;
+            this.ErrorText.Location = new System.Drawing.Point(15, 439);
+            this.ErrorText.Name = "ErrorText";
+            this.ErrorText.Size = new System.Drawing.Size(0, 23);
+            this.ErrorText.TabIndex = 7;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 21F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(72)))), ((int)(((byte)(83)))), ((int)(((byte)(108)))));
+            this.ClientSize = new System.Drawing.Size(687, 547);
+            this.Controls.Add(this.ErrorText);
+            this.Controls.Add(this.DownloadButton);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.OutputFolderButton);
+            this.Controls.Add(this.OutputFolderText);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Gentium Basic", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ForeColor = System.Drawing.Color.White;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Name = "MainForm";
+            this.Text = "Tumblr Backup";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox OutputFolderText;
+        private System.Windows.Forms.Button OutputFolderButton;
+        private System.Windows.Forms.Panel BlogNamePanel;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button DownloadButton;
+        private System.Windows.Forms.Label ErrorText;
+    }
+}
+
---
+++ MainForm.cs
@@ -0,0 +1,221 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Text;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.IO;
+
+namespace TumblrBackupGui
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+
+            OutputFolderDialog = new FolderBrowserDialog();
+        }
+
+        public FolderBrowserDialog OutputFolderDialog;
+        public List<TextBox> BlogTextBoxes = new List<TextBox>();
+        public List<Button> AddButtons = new List<Button>();
+        public List<Button> RemoveButtons = new List<Button>();
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            groupBox1.Controls.Add(BlogNamePanel);
+
+            //load fonts
+            PrivateFontCollection fontCollection = new PrivateFontCollection();
+
+            fontCollection.AddFontFile(@"Resources\GenBasB.ttf");
+            fontCollection.AddFontFile(@"Resources\GenBasR.ttf");
+
+            //set fonts
+            DownloadButton.Font = new Font((FontFamily)fontCollection.Families.GetValue(0), 14.0f);
+            label1.Font         = new Font((FontFamily)fontCollection.Families.GetValue(0), 12.0f);
+            groupBox1.Font      = new Font((FontFamily)fontCollection.Families.GetValue(0), 12.0f);
+
+            //output folder setup
+            OutputFolderText.Text = AppDomain.CurrentDomain.BaseDirectory;
+
+            OutputFolderText.KeyDown += MainForm_KeyDown;
+            OutputFolderButton.MouseDown += OutputFolderButton_Click;
+            DownloadButton.MouseDown += DownloadButton_Click;
+
+            for (int i = 0; i < 8; i++)
+            {
+                AddBlogBox(i);
+            }
+
+            AdjustBlogBoxPositions();
+        }
+
+        private void MainForm_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                InitiateDownload();
+            }
+        }
+
+        public void AddBlogBox(int index)
+        {
+            TextBox newBox = new TextBox();
+            Button newRemoveButton = new Button();
+            Button newAddButton = new Button();
+
+            newBox.KeyDown += MainForm_KeyDown;
+            newRemoveButton.MouseDown += RemoveButtonClicked;
+            newAddButton.MouseDown += AddButtonClicked;
+
+            BlogTextBoxes.Insert(index, newBox);
+            RemoveButtons.Insert(index, newRemoveButton);
+            AddButtons.Insert(index, newAddButton);
+
+            newRemoveButton.BackColor = Color.White;
+            newRemoveButton.BackgroundImage = Properties.Resources.minus;
+            newRemoveButton.BackgroundImageLayout = ImageLayout.Stretch;
+
+            newAddButton.BackColor = Color.White;
+            newAddButton.BackgroundImage = Properties.Resources.plus;
+            newAddButton.BackgroundImageLayout = ImageLayout.Stretch;
+
+            BlogNamePanel.Controls.Add(newBox);
+            BlogNamePanel.Controls.Add(newRemoveButton);
+            BlogNamePanel.Controls.Add(newAddButton);
+        }
+
+        public void RemoveBlogBox(int index)
+        {
+            if (index >= 0)
+            {
+                BlogTextBoxes[index].Text = "";
+
+                //Don't allow user to remove last text entry
+                if (BlogTextBoxes.Count > 1)
+                {
+                    BlogTextBoxes[index].Dispose();
+                    AddButtons[index].Dispose();
+                    RemoveButtons[index].Dispose();
+
+                    BlogTextBoxes.RemoveAt(index);
+                    AddButtons.RemoveAt(index);
+                    RemoveButtons.RemoveAt(index);
+                }
+            }
+        }
+
+        public void AdjustBlogBoxPositions()
+        {
+            for (int i = 0; i < BlogTextBoxes.Count; i++)
+            {
+                TextBox box = BlogTextBoxes[i];
+                Button add = AddButtons[i];
+                Button minus = RemoveButtons[i];
+
+                int YPosition = i * (box.Height + 5) + BlogNamePanel.AutoScrollPosition.Y;
+                int height = box.Height;
+
+                box.Location = new Point(0, YPosition);
+                box.Size = new Size(BlogNamePanel.Size.Width - 115, height);
+                box.TabIndex = BlogNamePanel.TabIndex + 1 + i;
+
+                minus.Location = new Point(box.Size.Width + 15, YPosition);
+                minus.Size = new Size(height, height);
+                minus.TabIndex = BlogNamePanel.TabIndex + 1 + BlogTextBoxes.Count + (i * 2);
+
+                add.Location = new Point(minus.Location.X + minus.Width + 5, YPosition);
+                add.Size = new Size(height, height);
+                add.TabIndex = BlogNamePanel.TabIndex + 1 + BlogTextBoxes.Count  + (i * 2) + 1;
+            }
+        }
+
+        private void OutputFolderButton_Click(object sender, EventArgs e)
+        {
+            DialogResult result = OutputFolderDialog.ShowDialog();
+
+            if (result == DialogResult.OK)
+            {
+                OutputFolderText.Text = OutputFolderDialog.SelectedPath;
+            }
+        }
+
+        private void AddButtonClicked(object sender, EventArgs e)
+        {
+            int index = AddButtons.IndexOf((Button)sender);
+
+            AddBlogBox(index + 1);
+            AdjustBlogBoxPositions();
+        }
+
+        private void RemoveButtonClicked(object sender, EventArgs e)
+        {
+            int index = RemoveButtons.IndexOf((Button)sender);
+
+            RemoveBlogBox(index);
+            AdjustBlogBoxPositions();
+
+        }
+
+        private void DownloadButton_Click(object sender, EventArgs e)
+        {
+            InitiateDownload();
+        }
+
+        public void InitiateDownload()
+        {
+            //clear previous errors away
+            ErrorText.Text = "";
+
+            string outputPath = OutputFolderText.Text;
+            string names = "";
+
+            for (int i = 0; i < BlogTextBoxes.Count; i++)
+            {
+                String blogName = BlogTextBoxes[i].Text;
+                bool validName = true;
+
+                blogName.Trim();
+
+                //valid conditions for blog name
+                validName &= blogName.IndexOfAny(new char[] { ' ', '~', '`', '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '_', '=', '+', '[', '{', ']', '}', '\\', '|', ';', ':', '\'', '\"', ',', '<', '.', '>', '/', '?' }) == -1;
+                validName &= blogName.Length > 0;
+
+                if (validName)
+                {
+                    names += blogName + " ";
+                    BlogTextBoxes[i].Text = "";
+                }
+                else if (blogName.Length > 0)
+                {
+                    ErrorText.Text += "\"" + @blogName + "\" is not a valid blog name!\n";
+                }
+            }
+
+            //ensure valid output path
+            if (outputPath.IndexOfAny(Path.GetInvalidPathChars()) != -1)
+            {
+                ErrorText.Text += "Invalid Output Path!\n";
+                return;
+            }
+            else if (outputPath.Length != 0 && !Directory.Exists(outputPath))
+            {
+                Directory.CreateDirectory(outputPath);
+            }
+
+            if (names.Length > 0)
+            {
+                ProcessStartInfo startInfo = new ProcessStartInfo()
+                {
+                    WorkingDirectory = @outputPath,
+                    FileName = AppDomain.CurrentDomain.BaseDirectory + "tumblr_backup/tumblr_backup.exe ",
+                    Arguments = @names
+                };
+
+                Process.Start(startInfo);
+            }
+        }
+    }
+}
